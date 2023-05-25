ec721059b7b360f5550819c5189b997580af3b17
Daniel Saulin <saulin.daniel33@gmail.com>
2023-05-25 19:38:23 +0300

    Сделал вместо консольного приложения библиотеку

---
+++ TestTask.Implementation/Test2.cs
@@ -0,0 +1,57 @@
+﻿using TestTasks;
+
+namespace TestTask.Implementation
+{
+    public class Test2 : ITest2
+    {
+        public void Case1_NotifyUserRegistered(string userName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int Case1_GetRegisteredUsersCount()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string Case2_NotifyMeter(string serialNumber)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Case2_MeterAlreadyExists(string serialNumber)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Case3_NotifyUserSecurityEvent(string userName, bool userLoggedIn)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int Case3_GetUserLoggedInCount(string userName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Case4_NotifyCommand(int commandCode)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int? Case4_TryExtractNextCommand()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Case5_PushContext(string contextDescription)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string Case5_PopContext()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
