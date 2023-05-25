dce5ce833dfbd35dd3d36932c445cd97069a28e4
Daniel Saulin <saulin.daniel33@gmail.com>
2023-05-25 18:59:57 +0300

    Реализовал одно задание в 5 тесте

---
+++ TestTask.Implementation/Test5.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using TestTasks;
+
+namespace TestTask.Implementation
+{
+    public class Test5 : ITest5
+    {
+        /// <summary>
+        /// Возвращает "все человечество", рожденное, начиная с переданного в качестве аргумента предка (включительно)
+        /// </summary>
+        /// <param name="oldestHuman"></param>
+        /// <returns>"все человечество"</returns>
+        public IEnumerable<HumanWithParent> EnumAllHuman(HumanWithChildren oldestHuman)
+        {
+            var humans = new List<HumanWithParent>();
+            EnumAllHumanRec(oldestHuman, null);
+            
+            void EnumAllHumanRec(HumanWithChildren human, HumanWithChildren parent)
+            {
+                var humanWithParent = new HumanWithParent()
+                {
+                    Parent = parent,
+                    Name = human.Name
+                };
+                humans.Add(humanWithParent);
+
+                if (human.Children == null || human.Children.Length == 0) return;
+                
+                foreach (var child in human.Children)
+                {
+                    EnumAllHumanRec(child, parent);
+                }
+            }
+
+            return humans;
+        }
+        
+        
+
+        public IEnumerable<HumanWithParent> EnumAllHuman(IEnumerable<HumanWithChildren> oldestHumanGroup)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
