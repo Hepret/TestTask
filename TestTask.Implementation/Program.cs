f802b5a7e4a6c1367c0834fb7234a1a2a0b7c2d0
Daniel Saulin <saulin.daniel33@gmail.com>
2023-05-25 15:15:00 +0300

    Создал проект для реализации заданий

---
+++ TestTask.Implementation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestTask.Implementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
